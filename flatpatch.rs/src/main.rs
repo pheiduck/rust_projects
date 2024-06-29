56b5a77ea400f4b11727e1af5336c327e52dc74f
Philip H <47042125+pheiduck@users.noreply.github.com>
2024-06-29 16:31:43 +0200

    more fixes

--- flatpatch.rs/src/main.rs
+++ flatpatch.rs/src/main.rs
@@ -6,9 +6,9 @@ use std::process::Command;
 fn main() {
     // Check Privileges
     if !Uid::effective().is_root() {
-        eprintln!("WARNING: \x1b[0mPlease run with sudo privileges.");
+        eprintln!("WARNING: Please run with sudo privileges.");
     } else {
-        let output = if cfg!(os_relase != "Ubuntu") {
+        let output = if cfg!(os_release != "Ubuntu") {
             eprintln!("ERROR: You are trying to install this on an unsupported distribution.\nOnly Ubuntu is supported.");
         } else {
             Command::new("sh")
@@ -16,6 +16,6 @@ fn main() {
             .arg("echo hello")
             .output()
             .expect("failed to execute process")
-        }
+        };
     }
-};
+}
