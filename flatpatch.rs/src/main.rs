3d747211c17daf0ca849ad72e995a5b90e6ea372
Philip H <47042125+pheiduck@users.noreply.github.com>
2024-06-29 16:27:39 +0200

    fixes

--- flatpatch.rs/src/main.rs
+++ flatpatch.rs/src/main.rs
@@ -3,17 +3,19 @@
 use nix::unistd::Uid;
 use std::process::Command;
 
+fn main() {
     // Check Privileges
-if !Uid::effective().is_root() {
-    eprintln!("WARNING: \x1b[0mPlease run with sudo privileges.");
-} else {
-let output = if cfg!(os_relase != "Ubuntu") {
-    eprintln!("ERROR: You are trying to install this on an unsupported distribution.\nOnly Ubuntu is supported.");
-} else {
-    Command::new("sh")
-        .arg("-c")
-        .arg("echo hello")
-        .output()
-        .expect("failed to execute process")
-}
-    };
+    if !Uid::effective().is_root() {
+        eprintln!("WARNING: \x1b[0mPlease run with sudo privileges.");
+    } else {
+        let output = if cfg!(os_relase != "Ubuntu") {
+            eprintln!("ERROR: You are trying to install this on an unsupported distribution.\nOnly Ubuntu is supported.");
+        } else {
+            Command::new("sh")
+            .arg("-c")
+            .arg("echo hello")
+            .output()
+            .expect("failed to execute process")
+        }
+    }
+};
